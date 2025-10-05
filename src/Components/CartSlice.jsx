809e0dbc673a7afd89688a4fd112eecb7bc1aeab
anas <a7nas.talika@gmail.com>
2025-10-05 11:16:23 -0400

    First change

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,12 @@
-
-const CartSlice = ({
- 
+import { createSlice } from '@reduxjs/toolkit';
+const initialState = {
+  cartItems: [],
+};
+const CartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+  }
 });
 
 
